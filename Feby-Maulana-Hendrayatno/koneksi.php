54edd85836c43efc6dab08ded50b203536c56eb6
Feby-Maulana-Hendryatno <hendrayatno26@gmail.com>
2021-06-14 15:57:17 +0700

    menghapus file

--- Feby-Maulana-Hendrayatno/koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-$koneksi = new mysqli ("localhost", "root", "", "angkringan-family");
-
-if (!$koneksi) {
-    die("Connection Failed");
-}
--- hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "koneksi.php";
-
-    $nama_pelanggan = $_GET['nama_pelanggan'];
-
-    $sql = " DELETE FROM tabel WHERE nama_pelanggan = '$nama_pelanggan'";
-    $pdo->exec($sql);
-?>
